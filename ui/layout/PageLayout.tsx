231ad294b1b55d7296e8f380a03eaaa12e349ba9
Eddy Nordica <71158253+EddyNordica@users.noreply.github.com>
2023-04-16 21:42:48 +0200

    Added document title

--- ui/layout/PageLayout.tsx
+++ ui/layout/PageLayout.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Head from "next/head";
 import { Header } from "@/ui/widgets/Header";
 import { Footer } from "@/ui/widgets/Footer";
 import { PageContainer } from "./PageContainer";
+import { useTranslation } from "next-i18next";
+import { Translations } from "@/data/locales";
 
 export const PageLayout = (props: React.PropsWithChildren) => {
+  const { t } = useTranslation();
   return (
     <>
+      <Head>
+        <title>{t(Translations.streamerAnniversaryTracker)}</title>
+      </Head>
       <Header />
       <div className="bg-gray-100">
         <PageContainer>
