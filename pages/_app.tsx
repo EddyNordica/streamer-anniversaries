c7af083ee4e475e20f568297763ed80a911ce612
Eddy Nordica <71158253+EddyNordica@users.noreply.github.com>
2023-04-23 14:01:35 +0200

    Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { appWithTranslation } from "next-i18next";
 import { PageLayout } from "@/ui/layout/PageLayout";
 import { TabsterContext, useCreateTabster } from "@/lib/tabster";
@@ -7,11 +8,14 @@ import { TabsterContext, useCreateTabster } from "@/lib/tabster";
 const App = ({ Component, pageProps }: AppProps) => {
   const tabster = useCreateTabster();
   return (
-    <TabsterContext.Provider value={tabster}>
-      <PageLayout>
-        <Component {...pageProps} />
-      </PageLayout>
-    </TabsterContext.Provider>
+    <>
+      <TabsterContext.Provider value={tabster}>
+        <PageLayout>
+          <Component {...pageProps} />
+        </PageLayout>
+      </TabsterContext.Provider>
+      <Analytics />
+    </>
   );
 };
 
